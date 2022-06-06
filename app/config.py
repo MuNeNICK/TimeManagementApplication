7fdaf8cdedb93f251bf9e65b2ca5880ce1cf8901
MuNeNICK <mune0127@munenick.me>
2022-06-06 14:00:08 +0900

    test

---
+++ app/config.py
@@ -0,0 +1,3 @@
+DEBUG = True
+SQLALCHEMY_DATABASE_URL = 'sqlite:///sample_flask.db'
+SQALCHEMY_TRACK_MODIFICATIONS = True
--- app/views.py
+++ app/views.py
@@ -7,4 +7,17 @@ def index():
 
 @app.route('/test')
 def test():
-    return render_template('/test.html')
+    chinman = {
+        'thing1': '男性器',
+        'thing2': '女性器',
+        'things': ['ちんこ','まんこ']
+    }
+    return render_template('/test.html',chinman=chinman)
+
+@app.route('/signin')
+def signin():
+    return render_template('/signin.html')
+
+@app.route('/login')
+def login():
+    return render_template('/login.html')
