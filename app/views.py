428c400d7dd72632642ae5e930ba6349565cc8a8
MuNeNICK <mune0127@munenick.me>
2022-06-13 13:22:01 +0900

    test

--- app/views.py
+++ app/views.py
@@ -1,7 +1,19 @@
-from flask import render_html  # 追加
-
-app = render_html(__name__)
+from flask import render_template
+from app import app 
 
 @app.route('/')
 def index():
-    return render_html('/index.html')
+    return render_template('/index.html')
+
+@app.route('/test')
+def test():
+    chinman = {
+        'thing1': '男性器',
+        'thing2': '女性器',
+        'things': ['ちんこ','まんこ']
+    }
+    return render_template('/test.html',chinman=chinman)
+
+@app.route('/kasuform')
+def kasuform():
+    return render_template('/kasuform.html')
